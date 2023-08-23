74bc665b3d06015e6495260e7da7b8ea6bdc06ef
mark <cheapshoterr@gmail.com>
2023-08-23 16:37:22 +0300

    start

---
+++ app01/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <input type="text" />
+      <button>Add Todo</button>
+
+    </div>
+  );
+}
+
+export default App;
