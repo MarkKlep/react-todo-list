4606881279e9fce3d148985ed7a675f261d29aae
mark <cheapshoterr@gmail.com>
2023-08-24 16:23:05 +0300

    add todo

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,11 +1,48 @@
 import './App.css';
 
+import { useState } from 'react';
+
 function App() {
+
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+
+  const handleAddTodo = () => {
+    if(text.trim()) {
+      setTodos(
+        [
+          ...todos,
+          {
+            id: Date.now(),
+            title: text,
+            completed: false
+          }
+        ]
+      );
+      setText('');
+    }
+  }
+
   return (
     <div className="App">
       
-      <input type="text" />
-      <button>Add Todo</button>
+      <input
+        type="text"
+        value={text}
+        onChange={e=>setText(e.target.value)} 
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+
+      <ul>
+        {
+          todos.map(todo=>
+          <li key={todo.id}>
+            <input type="checkbox" />
+            <span>{todo.title}</span>
+            <button className="deleteTodo">&times;</button>
+          </li>)
+        }
+      </ul>
 
     </div>
   );
